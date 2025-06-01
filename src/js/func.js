a36aab676583a6461479cdf9cc94562afcc95858
giparreira <gi.parreirinha@gmail.com>
2025-06-01 17:44:30 -0300

    fix/ menu hamburguer adicionado na pagina quiz e alertas, criação do menu.js

--- src/js/func.js
+++ src/js/func.js
@@ -24,17 +24,3 @@ window.addEventListener('resize', updateCarousel);
 window.addEventListener('load', updateCarousel);
 
 setInterval(nextSlide, 5000);
-
-// Js do menu
-const hamburger = document.getElementById('hamburger');
-const menu = document.getElementById('menu');
-
-if (hamburger && menu) {
-  hamburger.addEventListener('click', function() {
-    if (menu.style.display === 'flex') {
-      menu.style.display = 'none';
-    } else {
-      menu.style.display = 'flex';
-    }
-  });
-}
