ad7392b51b39b17466aba0ea0c015d60e340916d
giparreira <gi.parreirinha@gmail.com>
2025-06-03 23:08:59 -0300

    feature/temas

--- src/js/temas.js
+++ src/js/temas.js
@@ -1,35 +1,49 @@
+// const botaoEscuro = document.getElementById('temaEscuro');
+//     let modoEscuro = false;
+
+//     botaoEscuro.addEventListener('click', () => {
+//       modoEscuro = !modoEscuro;
+
+//       if (modoEscuro) {
+//         document.body.classList.add('modo-escuro');
+//       } 
+//     });
+
+// // --------------------------------------------------------
+// const botaoClaro = document.getElementById('temaClaro');
+// let modoClaro = false;
+
+// botaoClaro.addEventListener('click', () => {
+//   modoClaro = !modoClaro;
+  
+//   if (modoClaro) {
+//     document.body.classList.add('modo-claro');
+//   } 
+// });
+
+// // --------------------------------------------------------
+// const botaoRosa = document.getElementById('temaRosa');
+//     let modoRosa = false;
+
+//     botaoRosa.addEventListener('click', () => {
+//       modoRosa = !modoRosa;
+
+//       if (modoRosa) {
+//         document.body.classList.add('modo-rosa')
+//       } 
+//       else {
+//         document.body.classList.remove('modo-rosa')
+//       }
+//     });
 const botaoEscuro = document.getElementById('temaEscuro');
-    let modoEscuro = false;
-
-    botaoEscuro.addEventListener('click', () => {
-      modoEscuro = !modoEscuro;
-
-      if (modoEscuro) {
-        document.body.style.backgroundColor = '#062F4F';
-      } 
-    });
-
 const botaoClaro = document.getElementById('temaClaro');
-    let modoClaro = false;
-
-    botaoClaro.addEventListener('click', () => {
-      modoClaro = !modoClaro;
-
-      if (modoClaro) {
-        document.body.style.backgroundColor = '#ffffff';
-      } 
-    });
-
 const botaoRosa = document.getElementById('temaRosa');
-    let modoRosa = false;
 
-    botaoRosa.addEventListener('click', () => {
-      modoRosa = !modoRosa;
+function setTema(classe) {
+  document.body.classList.remove('modo-escuro', 'modo-claro', 'modo-rosa');
+  document.body.classList.add(classe);
+}
 
-      if (modoRosa) {
-        document.body.classList.add('modo-rosa')
-      } 
-      else {
-        document.body.classList.remove('modo-rosa')
-      }
-    });
+botaoEscuro.addEventListener('click', () => setTema('modo-escuro'));
+botaoClaro.addEventListener('click', () => setTema('modo-claro'));
+botaoRosa.addEventListener('click', () => setTema('modo-rosa'));
